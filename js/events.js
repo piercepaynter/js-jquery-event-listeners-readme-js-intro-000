02ebe42708ea052dfeed5a245b1d4ca50c78bc54
Pierce Paynter <paynterpierce@gmail.com>
2020-06-30 03:43:50 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt(){
 function pressIt(){
   $('form').on('keydown', function(key) {
   if(key.which == 71){
-      alert('s was pressed');
+      alert('g was pressed');
   }
 });
 }
