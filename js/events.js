403f696656cbfcde1c0d85252d7024ef11e690da
Pierce Paynter <paynterpierce@gmail.com>
2020-06-30 03:44:46 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,14 @@ function pressIt(){
 }
 
 function submitIt(){
-
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
 }
 /*
 function submitIt(){
